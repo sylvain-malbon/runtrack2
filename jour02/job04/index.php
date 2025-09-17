69ad3e3e0d7710421501ba8e142b07ff9b5cac80
sylvain-malbon <sylvain.malbon@laplateforme.io>
2025-09-17 15:23:34 +0200

    Finalisation de jour02/job04 + ajout de l'index pour job05

--- jour02/job04/index.php
+++ jour02/job04/index.php
@@ -1,17 +1,24 @@
 <?php
 /*
-Afficher les nombres de 1 à 100 en mettant un retour à la ligne entre chaque nombre
-(“<br />”) et en remplaçant certains nombres par un mot selon les conditions suivantes :
-● Si le nombre est un multiple de 3, affichez “Fizz”.
-● Si le nombre est un multiple de 5, affichez “Buzz”.
-● Si le nombre est un multiple de 3 et de 5, affichez “FizzBuzz”.
+Afficher les nombres de 1 à 100 avec un retour à la ligne (<br />).
+- Si le nombre est un multiple de 3 : afficher "Fizz"
+- Si le nombre est un multiple de 5 : afficher "Buzz"
+- Si le nombre est un multiple de 3 et de 5 : afficher "FizzBuzz"
 */
 
 $nombre_max = 100;
 
-for ($i = 0 ; $i <= $nombre_max ; $i++) {
-    echo '<br/>' ;
-} if {
-    $i %3 == 
+for ($i = 1; $i <= $nombre_max; $i++) {
+    if ($i % 3 === 0 && $i % 5 === 0) { // en premier
+        echo 'FizzBuzz';
+    } elseif ($i % 3 === 0) { // en second
+        echo 'Fizz';
+    } elseif ($i % 5 === 0) { // en 3e
+        echo 'Buzz';
+    } else {
+        echo $i;
+    }
+
+    echo '<br />';
 }
-}
+?>
---
+++ jour02/job05/index.php
@@ -0,0 +1,7 @@
+<?php
+/*
+Faire un algorithme qui affiche les nombres premiers jusqu’à 1000 en mettant un retour
+à la ligne entre chaque nombre (“<br />”).
+*/
+
+$variableANOmmer = 100;
