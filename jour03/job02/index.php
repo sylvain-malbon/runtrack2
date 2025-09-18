41c88b0142acfacd1a92e22e88b4521fd32d5d51
sylvain-malbon <sylvain.malbon@laplateforme.io>
2025-09-18 15:35:58 +0200

    Ajout des scripts : affichage des caractères un sur deux (job02) et des voyelles (job03)

--- jour03/job02/index.php
+++ jour03/job02/index.php
@@ -6,4 +6,13 @@ Créez une variable de type string nommée $str et affectez y le texte :
 Parcourir cette chaîne en affichant seulement un caractère sur deux.
 Ex. : Tu e ntnssrn edsdn etmscmelslre osl li.
 */
+// 📝 Déclaration de la chaîne
+$str = "Tous ces instants seront perdus dans le temps comme les larmes sous la pluie.";
+
+// 🔁 Boucle pour afficher un caractère sur deux, sans calculer la longueur
+$i = 0;
+while (isset($str[$i])) {
+    echo $str[$i];
+    $i += 2; // ⏩ On saute une lettre à chaque fois
+}
 ?>
--- jour03/job03/index.php
+++ jour03/job03/index.php
@@ -1,9 +1,35 @@
 <?php
 /*
 Job03
-Créez une variable de type string nommée $str et affectez y le texte :
+Crée une variable de type string nommée $str et affecte-lui le texte :
 “I'm sorry Dave I'm afraid I can't do that”.
-Créez un algorithme qui parcourt cette chaîne et affiche uniquement les voyelles.
+Crée un algorithme qui parcourt cette chaîne et affiche uniquement les voyelles.
 Ex. : IoyaeIaaiIaoa
 */
+
+$str = "I'm sorry Dave I'm afraid I can't do that";
+
+// 🔠 Tableau des voyelles en majuscules et minuscules
+$voyelles = ['a','e','i','o','u','y','A','E','I','O','U','Y'];
+
+$i = 0;
+while (isset($str[$i])) {
+
+    // 🔁 On vérifie manuellement si $str[$i] est une voyelle
+    $j = 0;
+    $estVoyelle = false;
+    while (isset($voyelles[$j])) {
+        if ($str[$i] === $voyelles[$j]) {
+            $estVoyelle = true;
+        }
+        $j++;
+    }
+
+    // ✅ Si c’est une voyelle, on l’affiche
+    if ($estVoyelle) {
+        echo $str[$i];
+    }
+
+    $i++;
+}
 ?>
