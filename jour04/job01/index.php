11b420f834af287856f83419f8917bc3be27ee4b
sylvain-malbon <sylvain.malbon@laplateforme.io>
2025-09-22 09:12:51 +0200

    Ajout consignes jour04/job01 en com

---
+++ jour04/job01/index.php
@@ -0,0 +1,9 @@
+/*
+Job 01
+Développez un algorithme qui affiche le nombre d’arguments $_GET.
+Tips :
+Pour tester votre code, créez un formulaire html <form> de type GET avec différents
+champs <input> de type “text” et un <input> de type “submit” pour l’envoi.
+Vous pouvez ensuite afficher avec echo directement dans votre page par exemple :
+“Le nombre d’argument GET envoyé est : “
+*/
