f370d0f4d3740db7c375b054a7d54d65cb8ed7b6
sylvain-malbon <sylvain.malbon@laplateforme.io>
2025-09-23 15:47:35 +0200

    Finalisation du code dans job03/index.php

--- jour07/job03/index.php
+++ jour07/job03/index.php
@@ -6,4 +6,10 @@ Créez une fonction nommée “getHello()”.
 Cette fonction doit retourner “Hello LaPlateforme!”.
 Appelez cette fonction dans votre page en récupérant sa valeur de retour et affichez-la.
 */
+
+function gethello() {
+return "Hello LaPlateforme!";
+}
+echo gethello();
+
 ?>
