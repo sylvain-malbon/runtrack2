bd89028a51e94d159e6641702b4e754456f2fca9
sylvain-malbon <sylvain.malbon@laplateforme.io>
2025-09-24 10:44:43 +0200

    Finalisation du code dans job06/index.php

--- jour07/job06/index.php
+++ jour07/job06/index.php
@@ -14,4 +14,38 @@ qu’elle doit la modifier de sorte à ce que :
 ● les “T” des “7”.
 Cela est valable que l’on crie ou non (majuscules et minuscules).
 */
+
+function leetspeak($str) {
+    // le tableau avant les var
+    $leet = [ 
+        "A" => "4", "a" => "4",
+        "B" => "8", "b" => "8",
+        "E" => "3", "e" => "3",
+        "G" => "6", "g" => "6",
+        "L" => "1", "l" => "1",
+        "S" => "5", "s" => "5",
+        "T" => "7", "t" => "7"
+    ];
+
+    $i = 0; // init
+    $result = ""; // var string
+
+    while (isset($str[$i])) { // encore (isset($str[$i])
+        $char = $str[$i]; // on récupère le caractère
+
+        if (isset($leet[$char])) {
+            $result = $result . $leet[$char]; // remplacement leetspeak
+        } else {
+            $result = $result . $char; // quand caractère inchangé, sinon plante
+        }
+
+        $i++; // on passe au suivant (boucle)
+    }
+
+    return $result; // normal
+}
+
+// vérif possible
+$str = "Salut à tous ! Le Grand Test commence bientôt... Soyez prêts.";
+echo leetspeak($str); // Affiche en leet
 ?>
