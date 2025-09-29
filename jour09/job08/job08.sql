0fd39f5b8672de936e4c4f3c680db7f897eded94
sylvain-malbon <sylvain.malbon@laplateforme.io>
2025-09-29 10:10:15 +0200

    docs: ajustement du commentaire COUNT(*) dans job08 (jour09)

--- jour09/job08/job08.sql
+++ jour09/job08/job08.sql
@@ -1,6 +1,6 @@
 -- ❌ COUNT * FROM etudiants → syntaxe invalide
 -- ❌ COUNT FROM etudiant ... → il manque SELECT
--- ✅ En SQL, COUNT est une fonction → elle doit être appelée avec SELECT
+-- ✅ En SQL, COUNT() est une fonction → elle doit être appelée avec SELECT
 -- ✅ Le * est un paramètre de COUNT() → il doit être placé entre parenthèses : COUNT(*)
 
 SELECT COUNT(*) FROM etudiants;
